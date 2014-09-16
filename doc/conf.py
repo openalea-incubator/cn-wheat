7d821149e00e7af8d9e36f59524c106b58c4de25
cchambon <20965772+cachambon@users.noreply.github.com>
2014-09-16 11:55:15 +0000

    Activate the extension 'sphinx.ext.intersphinx' and add doc for post_processings.

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -240,3 +240,8 @@ texinfo_documents = [
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
+
+intersphinx_mapping = {'python': ('http://docs.python.org/2', None), 
+                       'pandas': ('http://pandas.pydata.org/pandas-docs/stable/', None),
+                       'numpy': ('http://docs.scipy.org/doc/numpy/', None),
+                       'scipy': ('http://docs.scipy.org/doc/scipy/reference/', None)}
--- src/cnwheat/post_processings.py
+++ src/cnwheat/post_processings.py
@@ -13,6 +13,18 @@ def plot_linear_regression(ModelMaker_array, python_array, plot_filepath=None):
     '''Perform a linear regression of `ModelMaker_array` vs `python_array` 
     and create a plot showing the fit against the original data.
     If `plot_filepath` is not None, save the plot to a PNG file.
+    
+    This function permits to compare the output computed using ModelMaker and the 
+    output computed using :func:`scipy.integrate.odeint`.
+    
+    :Parameters:
+    
+        - `ModelMaker_array` (:class:`numpy.ndarray`) - The output computed using ModelMaker.
+        
+        - `python_array` (:class:`numpy.ndarray`) - The output computed using :func:`scipy.integrate.odeint`.
+        
+        - `plot_filepath` (:class:`str`) - The file path to save the plot in. 
+        
     '''
     # Perform fit
     (aCoeff, bCoeff, rVal, pVal, stdError) = stats.linregress(ModelMaker_array, python_array)
