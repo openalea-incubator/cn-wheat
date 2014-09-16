69362d0f8bb493352a4abcac24cc5ea6b670adba
cchambon <20965772+cachambon@users.noreply.github.com>
2014-09-16 11:55:14 +0000

    Add postprocessing module.

---
+++ src/post_processings.py
@@ -0,0 +1,40 @@
+# -*- coding: latin-1 -*-
+'''
+Created on 2 sept. 2014
+
+@author: cchambon, adapted from http://www.landmap.ac.uk/index.php/Learning-Materials/Python-Scripting/6.4-Fitting-linear-equations#sthash.wDZ5zBrD.dpuf
+'''
+
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+
+def plot_linear_regression(ModelMaker_array, python_array, plot_filepath=None):
+    '''Perform a linear regression of then `ModelMaker_array` vs `python_array` 
+    and create a plot showing the fit against the original data.
+    If `plot_filepath` is not None, save the plot to a PNG file.
+    '''
+    # Perform fit
+    (aCoeff, bCoeff, rVal, pVal, stdError) = stats.linregress(ModelMaker_array, python_array)
+    
+    # Use fits to predict Python output for a range of diameters
+    ModelMaker_samples_array = np.linspace(min(ModelMaker_array), max(ModelMaker_array), 1000)
+    python_predict_array = aCoeff * ModelMaker_samples_array + bCoeff
+
+    # Create a string, showing the form of the equation (with fitted coefficients) and r squared value
+    # Coefficients are rounded to two decimal places.
+    equation = str(round(aCoeff,2)) + 'x + ' + str(round(bCoeff,2)) + ' (r$^2$ = ' + str(round(rVal**2,2)) + ')'
+
+    # Plot fit against original data
+    plt.plot(ModelMaker_array, python_array,'.')
+    plt.plot(ModelMaker_samples_array, python_predict_array)
+    plt.xlabel('ModelMaker')
+    plt.ylabel('Python')
+    plt.legend(['ModelMaker vs Python', equation])
+
+    # Save plot
+    if plot_filepath is not None:
+        plt.savefig(plot_filepath, dpi=200, format='PNG')
+
+
+
