0cbe6d3ed6751035259b5b665e7aef5b9a602591
cchambon <cchambon@576115c9-5df5-405b-b3c1-aafd07cad693>
2014-09-16 11:55:14 +0000

    Implement a class lamina.

--- trunk/test/test_cn_model.py
+++ trunk/test/test_cn_model.py
@@ -1,6 +1,8 @@
 '''
 Test cn_model.py. 
 
+CSV files must contain only ASCII characters and ',' as separator.
+
 Be sure to add the 'src' directory to your PYTHONPATH before running this script.
 '''
 
@@ -9,34 +11,36 @@ import os
 import numpy as np
 import pandas as pd
 
+import lamina
 import cn_model
 
 data_dirpath = 'data'
-input_filepath = os.path.join(data_dirpath, 'input.csv') # ASCII characters only and ',' as separator
-modelmaker_output_filepath = os.path.join(data_dirpath, 'modelmaker_output.csv')
-python_output_filepath = os.path.join(data_dirpath, 'python_output.csv')
+ModelMaker_output_filename = 'ModelMaker_output.csv'
+python_output_filename = 'python_output.csv'
 
 relative_tolerance = 10e-3
 absolute_tolerance = 10e-3
 
-def test_run():
-    input_df = pd.read_csv(input_filepath, index_col='t')
+def test_1_lamina():
+    curr_data_dirpath = os.path.join(data_dirpath, '1_lamina')
+    Assimilation_filepath = os.path.join(curr_data_dirpath, 'Assimilation_lamina1.csv')
+    ModelMaker_output_filepath = os.path.join(curr_data_dirpath, ModelMaker_output_filename)
+    python_output_filepath = os.path.join(curr_data_dirpath, python_output_filename)
     
-    # initial conditions
-    STORAGE_0 = 0
-    SUCROSE_lamina_0 = 0
-    TRIOSESP_0 = 0
-    y0 = [STORAGE_0, SUCROSE_lamina_0, TRIOSESP_0]  
+    Assimilation_df = pd.read_csv(Assimilation_filepath, index_col='t')
     
+    lamina_1 = lamina.Lamina(lamina_area=0.00346, Mstruct_lamina=0.14, Assimilation=Assimilation_df.An, 
+                           STORAGE_0=0, SUCROSE_lamina_0=0, TRIOSESP_0=0)
+
     # run the model
-    python_output_df = cn_model.run(start_time=0, stop_time=960, number_of_output_steps=241,
-                                    initial_conditions=y0, An=input_df.An)
+    python_output_df = cn_model.run(start_time=0, stop_time=960, number_of_output_steps=241, lamina_1=lamina_1)
     python_output_df.to_csv(python_output_filepath, na_rep='NA', index=False)
     
     # compare to the output of ModelMaker
-    modelmaker_output_df = pd.read_csv(modelmaker_output_filepath)
-    np.testing.assert_allclose(python_output_df.values, modelmaker_output_df.values, relative_tolerance, absolute_tolerance)
-    
+    ModelMaker_output_df = pd.read_csv(ModelMaker_output_filepath)
+    np.testing.assert_allclose(python_output_df.values, ModelMaker_output_df.values, relative_tolerance, absolute_tolerance)
+     
+     
 if __name__ == '__main__':
-    test_run()
+    test_1_lamina()
 
