3a5233d0b8ff17596c8f37c631d54f0d0ae1758c
rbarillot <rbarillot@576115c9-5df5-405b-b3c1-aafd07cad693>
2015-02-06 14:45:32 +0000

    - Improvement of parameters calibration - Add Carbon molar mass as a parameter

--- trunk/cnwheat/parameters.py
+++ trunk/cnwheat/parameters.py
@@ -8,7 +8,7 @@
 
     :copyright: Copyright 2014 INRA-EGC, see AUTHORS.
     :license: TODO, see LICENSE for details.
-    
+
     .. seealso:: Barillot et al. 2014.
 """
 
@@ -29,6 +29,7 @@ class OrganParameters:
     ALPHA_AXIS = 1                          #: Proportion of the structural mass containing the substrates
     DELTA_T = 3600                          #: Timestep of the model (s)
 
+    C_MOLAR_MASS = 12                       #: Molar mass of carbon (g mol-1)
     AMINO_ACIDS_C_RATIO = 3.67              #: Mean ratio C:N in the major amino acids of plants (Glu, Gln, Ser, Asp, Ala, Gly)
     AMINO_ACIDS_N_RATIO = 1.17              #: Mean number of mol of N in 1 mol of the major amino acids of plants (Glu, Gln, Ser, Asp, Ala, Gly)
     AMINO_ACIDS_MOLAR_MASS_N_RATIO = 0.145  #: Mean contribution of N in amino acids mass
@@ -41,36 +42,36 @@ class PhotosyntheticOrganParameters(OrganParameters):
     Constants of photosynthetic organs.
     """
     # Sucrose
-    VMAX_SUCROSE = 1                #: Maximal rate of sucrose synthesis (Âµmol C s-1 g-1 MS)
-    K_SUCROSE = 0.66                #: Affinity coefficient of sucrose synthesis (Âµmol C g-1 MS)
+    VMAX_SUCROSE = 1                #: Maximal rate of sucrose synthesis (µmol C s-1 g-1 MS)
+    K_SUCROSE = 0.66                #: Affinity coefficient of sucrose synthesis (µmol C g-1 MS)
 
     # Starch
-    VMAX_STARCH = 2                 #: Maximal rate of starch synthesis (Âµmol C s-1 g-1 MS)
-    K_STARCH = 20                   #: Affinity coefficient of starch synthesis (Âµmol C g-1 MS)
+    VMAX_STARCH = 2                 #: Maximal rate of starch synthesis (µmol C s-1 g-1 MS)
+    K_STARCH = 20                   #: Affinity coefficient of starch synthesis (µmol C g-1 MS)
     DELTA_DSTARCH = 0.0001          #: Relative rate of starch degradation (s-1)
 
     # Fructans
-    VMAX_SFRUCTAN = 0.2             #: Maximal rate of fructan synthesis (Âµmol C s-1 g-1 MS)
-    K_SFRUCTAN = 5000               #: Affinity coefficient of fructan synthesis (Âµmol C g-1 MS)
+    VMAX_SFRUCTAN = 0.2             #: Maximal rate of fructan synthesis (µmol C s-1 g-1 MS)
+    K_SFRUCTAN = 5000               #: Affinity coefficient of fructan synthesis (µmol C g-1 MS)
     N_SFRUCTAN = 3                  #: Number of "substrates" for fructan synthesis (dimensionless)
     VMAX_REGUL_SFRUCTAN = 1         #: Maximal value of the regulation function of fructan synthesis (dimensionless)
     K_REGUL_SFRUCTAN = 8            #: Affinity coefficient of the regulation function of fructan synthesis (dimensionless)
     N_REGUL_SFRUCTAN = 15           #: Parameter of the regulation function of fructan synthesis (dimensionless)
-    VMAX_DFRUCTAN = 0.035           #: Maximal rate of fructan degradation (Âµmol C s-1 g-1 MS)
-    K_DFRUCTAN = 100                #: Affinity coefficient of fructan degradation (Âµmol C g-1 MS)
+    VMAX_DFRUCTAN = 0.035           #: Maximal rate of fructan degradation (µmol C s-1 g-1 MS)
+    K_DFRUCTAN = 100                #: Affinity coefficient of fructan degradation (µmol C g-1 MS)
 
     # Loading sucrose
     SIGMA = 1.85e-07                #: Conductivity of an organ-phloem pathway (g mol-1 m-2 s-1) ; used to compute the sucrose loaded to the phloem
     BETA = 1                        #: Kind of volumetric mass density at power -2/3 ((g m-3)**(-2/3)) ; used to compute the sucrose loaded to the phloem
 
     # Amino acids
-    VMAX_AMINO_ACIDS = 0.001        #: Maximal rate of amino acid synthesis (Âµmol N s-1 g-1 MS)
-    K_AMINO_ACIDS_NITRATES = 5      #: Affinity coefficient of amino acid synthesis from nitrates (Âµmol N g-1 MS)
-    K_AMINO_ACIDS_TRIOSESP = 0.1    #: Affinity coefficient of amino acid synthesis from triosesP (Âµmol C g-1 MS)
+    VMAX_AMINO_ACIDS = 0.05          #: Maximal rate of amino acid synthesis (µmol N s-1 g-1 MS)
+    K_AMINO_ACIDS_NITRATES = 3      #: Affinity coefficient of amino acid synthesis from nitrates (µmol N g-1 MS)
+    K_AMINO_ACIDS_TRIOSESP = 0.1    #: Affinity coefficient of amino acid synthesis from triosesP (µmol C g-1 MS)
 
     # Proteins
-    VMAX_SPROTEINS = 1.E-02         #: Maximal rate of protein synthesis (Âµmol N s-1 g-1 MS)
-    K_SPROTEINS = 20                #: Affinity coefficient of protein synthesis (Âµmol N g-1 MS)
+    VMAX_SPROTEINS = 0.03      #0.04 trop de synthese    #: Maximal rate of protein synthesis (µmol N s-1 g-1 MS)
+    K_SPROTEINS = 20                #: Affinity coefficient of protein synthesis (µmol N g-1 MS)
     DELTA_DPROTEINS = 1.85E-6       #: Relative rate of protein degradation (s-1)
 
 
@@ -132,8 +133,8 @@ class GrainsParameters(OrganParameters):
     K_RGR = 300             #: Affinity coefficient of the Relative Growth Rate of grain structure (dimensionless)
 
     # Starch parameters
-    VMAX_STARCH = 0.5       #: Maximal rate of grain filling of starch (Âµmol C s-1 g-1 MS)
-    K_STARCH = 100          #: Affinity coefficient of grain filling of starch (Âµmol C g-1 MS)
+    VMAX_STARCH = 0.75      #: Maximal rate of grain filling of starch (µmol C s-1 g-1 MS)
+    K_STARCH = 100          #: Affinity coefficient of grain filling of starch (µmol C g-1 MS)
 
     Y_GRAINS = 0.75         #: Proportion of C loaded from phloem actually used for grain structure and starch (1 - Y_GRAINS is a kind of growth respiration)
     FILLING_INIT = 360      #: Time (h) at which phloem loading switch from grain structure to accumulation of starch
@@ -145,10 +146,10 @@ class RootsParameters(OrganParameters):
     """
     ALPHA = 1                                 #: Proportion of structural mass containing substrate
 
-    VMAX_SUCROSE_UNLOADING = 0.015            #: Maximal rate of sucrose unloading from phloem to roots (Âµmol C sucrose s-1 g-1 MS)
-    K_SUCROSE_UNLOADING = 100                 #: Affinity coefficient of sucrose unloading from phloem to roots (Âµmol C sucrose g-1 MS)
-    VMAX_AMINO_ACIDS_UNLOADING = 0.015        #: Maximal rate of amino acids unloading from phloem to roots (Âµmol N amino acids s-1 g-1 MS)
-    K_AMINO_ACIDS_UNLOADING = 100             #: Affinity coefficient of amino acids unloading from phloem to roots (Âµmol N amino acids g-1 MS)
+    VMAX_SUCROSE_UNLOADING = 0.02             #: Maximal rate of sucrose unloading from phloem to roots (µmol C sucrose s-1 g-1 MS)
+    K_SUCROSE_UNLOADING = 100                 #: Affinity coefficient of sucrose unloading from phloem to roots (µmol C sucrose g-1 MS)
+    VMAX_AMINO_ACIDS_UNLOADING = 0.01         #: Maximal rate of amino acids unloading from phloem to roots (µmol N amino acids s-1 g-1 MS)
+    K_AMINO_ACIDS_UNLOADING = 100             #: Affinity coefficient of amino acids unloading from phloem to roots (µmol N amino acids g-1 MS)
 
     # Nitrate uptake
     K_TR_UPTAKE_NITRATES = 1E-2     #: Affinity coefficient of nitrate uptake by roots (mm H20)
@@ -157,12 +158,12 @@ class RootsParameters(OrganParameters):
     A_K_HATS = 211812               #: Parameter for estimating the affinity coefficient of nitrates uptake at saturating soil N concentration;HATS (dimsensionless)
     LAMBDA_K_HATS = 0.0018          #: Parameter for estimating the affinity coefficient of nitrates uptake at saturating soil N concentration;HATS (g m-3)
     A_LATS = 4.614E-09              #: Parameter for estimating the rate of nitrates uptake at low soil N concentration; LATS (dimensionless)
-    LAMBDA_LATS = 1.6517E-03        #: Parameter for estimating the rate of nitrates uptake at low soil N concentration; LATS (m3 Âµmol-1 s-1)
+    LAMBDA_LATS = 1.6517E-03        #: Parameter for estimating the rate of nitrates uptake at low soil N concentration; LATS (m3 µmol-1 s-1)
 
     # Amino acids
-    VMAX_AMINO_ACIDS = 1            #: Maximal rate of amino acid synthesis (Âµmol N s-1 g-1 MS)
-    K_AMINO_ACIDS_NITRATES = 5      #: Affinity coefficient of amino acid synthesis from nitrates (Âµmol N g-1 MS)
-    K_AMINO_ACIDS_SUCROSE = 0.01    #: Affinity coefficient of amino acid synthesis from triosesP (Âµmol C g-1 MS)
-    K_TR_EXPORT_AMINO_ACIDS = 1E-2  #: Affinity coefficient of amino acids export from roots to shoot (mm H20)
+    VMAX_AMINO_ACIDS = 0.005        #: Maximal rate of amino acid synthesis (µmol N s-1 g-1 MS)
+    K_AMINO_ACIDS_NITRATES = 3      #: Affinity coefficient of amino acid synthesis from nitrates (µmol N g-1 MS)
+    K_AMINO_ACIDS_SUCROSE = 3       #: Affinity coefficient of amino acid synthesis from triosesP (µmol C g-1 MS)
+    K_TR_EXPORT_AMINO_ACIDS = 1E-8  #: Affinity coefficient of amino acids export from roots to shoot (mm H20)
 
 
